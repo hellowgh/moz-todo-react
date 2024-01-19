eb0a7a1b38f8b49989b8f5df9114f6d201de1ba9
hellowgh <hellowgh@qq.com>
2024-01-20 07:21:32 +0800

    完成过滤

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -52,7 +52,7 @@ function Todo(props) {
           id={props.id}
           type="checkbox"
           defaultChecked={props.completed}
-          onChange={() => props.toggleTaskCompleted(props.id)}
+          onChange={() => props.toggleTaskCompeted(props.id)}
         />
         <label className="todo-label" htmlFor={props.id}>
           {props.name}
