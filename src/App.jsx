3df4ca95f241db05a3912309cb31b586dd02e1f7
hellowgh <hellowgh@qq.com>
2024-01-20 06:22:24 +0800

    完成过滤

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Todo from "./components/Todo";
 import {useState} from "react";
 import {nanoid} from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  completed: (task) => task.completed,
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
   function toggleTaskCompeted(id) {
     const updateList = tasks.map(t => {
@@ -36,16 +44,18 @@ function App(props) {
   }
 
 
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompeted={toggleTaskCompeted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
+  const taskList = tasks
+    .filter(task => FILTER_MAP[filter](task))
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompeted={toggleTaskCompeted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
   ));
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
@@ -63,14 +73,21 @@ function App(props) {
     setTasks(remaining);
   }
 
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
